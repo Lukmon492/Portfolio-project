8e5feba61997defec92d4365fa808de8f73bf0d9
Lukmon492 <lukmon492@gmail.com>
2022-12-15 22:27:22 +0100

    modifications

--- New-portfolio/src/components/about/About.jsx
+++ New-portfolio/src/components/about/About.jsx
@@ -1,10 +1,51 @@
 import React from "react";
 import "./about.css";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+import ME from "../../assets/me-about.jpg";
 
 const About = () => {
   return (
-    <section id="about" className="btn-primary">
-      About
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>2+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>60+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet
+            ipsum adipisci suscipit delectus? Obcaecati quaerat voluptatibus
+            molestiae tempora at beatae unde accusantium fugiat hic libero!
+            Laudantium soluta facere beatae et.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
